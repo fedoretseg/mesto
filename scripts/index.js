965286f0f324dde25c8b0554bbc374fbf2be3567
Egor Fedorets <fedoretseg@yandex.ru>
2021-04-22 16:48:08 +0300

    Practical work 4-4

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,39 @@
-let openPopupEditbutton = document.querySelector('.profile__editbutton');
-let popup = document.querySelector('.popup');
-let closePopupEditbutton = document.querySelector('.popup__close');
-let profileCaption = document.querySelector('.profile__caption');
-let saveButtonPopup = document.querySelector('.popup__form_submit');
-let formElement = document.querySelector('.popup__form');
+let openPopupEditbutton = document.querySelector(".profile__editbutton");
+let popup = document.querySelector(".popup");
+let closePopupEditbutton = document.querySelector(".popup__close");
+let profileCaption = document.querySelector(".profile__caption");
+let saveButtonPopup = document.querySelector(".popup__form_submit");
+let formElement = document.querySelector(".popup__form");
 
-let jobInput = document.querySelector('.popup__form_job-input');
-let nameInput = document.querySelector('.popup__form_name-input');
+let jobInput = document.querySelector(".popup__form_job-input");
+let nameInput = document.querySelector(".popup__form_name-input");
 
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
+let profileName = document.querySelector(".profile__name");
+let profileProfession = document.querySelector(".profile__profession");
 
 function togglePopupEditbutton(evt) {
-	evt.preventDefault();
-	popup.classList.toggle('popup_opened');
-};
+    evt.preventDefault();
+    popup.classList.toggle("popup_opened");
+}
 
-openPopupEditbutton.addEventListener('click', togglePopupEditbutton);
+openPopupEditbutton.addEventListener("click", togglePopupEditbutton);
 
-closePopupEditbutton.addEventListener('click', togglePopupEditbutton);
+closePopupEditbutton.addEventListener("click", togglePopupEditbutton);
 
-popup.addEventListener('click', function(evt) {
-	if (evt.target === evt.currentTarget) {
-		togglePopupEditbutton(evt);
-	}
-
+popup.addEventListener("click", function (evt) {
+    if (evt.target === evt.currentTarget) {
+        togglePopupEditbutton(evt);
+    }
 });
 
-
 function editProfile(evt) {
-	evt.preventDefault();
-	profileName.textContent = `${nameInput.value}`;
+    evt.preventDefault();
+    profileName.textContent = `${nameInput.value}`;
     profileProfession.textContent = `${jobInput.value}`;
 }
 
-formElement.addEventListener('submit', togglePopupEditbutton);
-formElement.addEventListener('submit', editProfile);
+formElement.addEventListener("submit", togglePopupEditbutton);
+formElement.addEventListener("submit", editProfile);
 
 
 
