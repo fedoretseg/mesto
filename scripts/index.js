97c829b9c95cca092093fbda47b0e170ce63997c
Egor Fedorets <fedoretseg@yandex.ru>
2021-04-23 21:51:17 +0300

    Practical work 4-7

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,41 @@
 let openPopupEditbutton = document.querySelector(".profile__editbutton");
 let popup = document.querySelector(".popup");
 let closePopupEditbutton = document.querySelector(".popup__close");
+let profileCaption = document.querySelector(".profile__caption"); 
 let formElement = document.querySelector(".popup__form");
-let jobInput = document.querySelector(".popup__form_job-input");
-let nameInput = document.querySelector(".popup__form_name-input");
+let nameInput = document.querySelector(".popup__input-name");
+let jobInput = document.querySelector(".popup__input-job");
 let profileName = document.querySelector(".profile__name");
 let profileProfession = document.querySelector(".profile__profession");
 
+//открываем popup
 function togglePopupEditbutton(evt) {
     evt.preventDefault();
     popup.classList.toggle("popup_opened");
+    profileCaption.insertAdjacentHTML(`
+    	<h1 class="profile__name">${profileName.textContent}</h1>
+        <p class="profile__profession">${profileProfession.textContent}</p>`)
+    // nameInput.value = profileName.textContent;
+    // jobInput.value = profileProfession.textContent;
 }
-
-openPopupEditbutton.addEventListener("click", togglePopupEditbutton);
-
-closePopupEditbutton.addEventListener("click", togglePopupEditbutton);
-
+//закрываем popup без изменения данных
 popup.addEventListener("click", function (evt) {
     if (evt.target === evt.currentTarget) {
         togglePopupEditbutton(evt);
     }
 });
 
+//закрываем popup с сохранением измененных данных
 function editProfile(evt) {
     evt.preventDefault();
-    profileName.textContent = `${nameInput.value}`;
-    profileProfession.textContent = `${jobInput.value}`;
+    profileName.textContent = nameInput.value;
+    profileProfession.textContent = jobInput.value;
 }
 
-formElement.addEventListener("submit", togglePopupEditbutton);
+//отрабатываем действия
+openPopupEditbutton.addEventListener("click", togglePopupEditbutton);
+closePopupEditbutton.addEventListener("click", togglePopupEditbutton);
+// formElement.addEventListener("submit", togglePopupEditbutton);
 formElement.addEventListener("submit", editProfile);
 
 
